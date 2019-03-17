3097131253ff11862883c1b13d9a2046a245ba0a
AksshatGupta <aksshatyashgupta@gmail.com>
2019-03-17 12:04:15 -0400

    sass and hbs

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const path = require('path');
-const less = require('gulp-less');
+const sass = require('gulp-sass');
 const gulp = require('gulp');
+
+sass.compiler = require("node-sass");
 
 function defaultTask(done) {
   // place code for your default task here
@@ -9,10 +11,12 @@ function defaultTask(done) {
 
 gulp.task('default', defaultTask);
 
-gulp.task('less', () => gulp.src('./src/*.less')
-  .pipe(less({
-    paths: [path.join(__dirname, 'less', 'includes')],
-  }))
-  .pipe(gulp.dest('./dist/css')));
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+});
 
-gulp.watch('src/*.less', gulp.parallel('less'));
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 const express = require('express');
+const hbs = require('express-hbs');
 
 const app = express();
 const path = require('path');
 
+function relative(fp) {
+  return path.join(__dirname, fp);
+}
+
 const port = 3000;
 
-app.get('/', (req, res) => res.sendFile(path.join(`${__dirname}/dist/index.html`)));
+app.get('/', (req, res) => res.render('index', {
+  title: 'home',
+  homePage: 'This is the home page',
+}));
+
+app.get('/about', (req, res) => res.render('about', {
+  title: 'about',
+}));
+
+app.get('/blog', (req, res) => res.render('blog', {
+  title: 'blog',
+}));
 
 /*eslint-disable*/
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 /* eslint-enable */
 
-app.use('/static', express.static(path.join(__dirname, 'dist')));
+//app.use('/static', express.static(path.join(__dirname, 'dist')));
+app.use(express.static(__dirname + "/dist"));
+
+app.engine('hbs', hbs.express4({
+  partialsDir: [relative('views/partials')],
+  layoutsDir: relative('views/layouts'),
+  defaultLayout: relative('views/layouts/main.hbs'),
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', `${__dirname}/views`);
